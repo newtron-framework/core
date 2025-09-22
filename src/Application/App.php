18a24336bdeb92c9adfbff66b91d09883ce4420d
James <78121730+itsnewtjam@users.noreply.github.com>
2025-09-21 20:30:08 -0400

    add env loading, root path

--- src/Application/App.php
+++ src/Application/App.php
@@ -12,15 +12,17 @@ class App {
   private static Container $container;
   private static ServiceProviderRegistry $serviceProviderRegistry;
 
-  private function __construct() {
+  private function __construct(string $rootPath) {
+    $this->loadEnv();
+
     $this->container = new Container();
     $this->serviceProviderRegistry = new ServiceProviderRegistry($this->container);
     $this->registerServices();
   }
 
-  public static function getInstance(): self {
+  public static function create(string $root): self {
     if (!isset(self::$instance)) {
-      self::$instance = new self;
+      self::$instance = new self($root);
     }
 
     return self::$instance;
@@ -40,6 +42,41 @@ class App {
 
   public static function getContainer(): Container {
     return self::$container;
+  }
+
+  private function loadEnv(): void {
+    $path = $this->rootPath . '/.env';
+
+    if (!file_exists($path)) {
+      return;
+    }
+
+    if (!is_readable($path)) {
+      return;
+    }
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+      if (strpos(trim($line), '#') === 0) {
+        continue;
+      }
+
+      [$key, $value] = explode('=', $line, 2);
+      $key = trim($key);
+      $value = trim($value);
+
+      if (preg_match('/"(.*)"/', $value, $matches)) {
+        $value = $matches[1];
+      } elseif (preg_match('/\'(.*)\'/', $value, $matches)) {
+        $value = $matches[1];
+      }
+
+      if (!array_key_exists($key, $_SERVER) && !array_key_exists($key, $_ENV)) {
+        putenv(sprintf('%s=%s', $key, $value));
+        $_SERVER[$key] = $value;
+        $_ENV[$key] = $value;
+      }
+    }
   }
 
   private function registerServices(): void {
