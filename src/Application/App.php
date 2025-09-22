ccb1af79a4b7db0312fc94bc2a6c2e89a764d000
James <78121730+itsnewtjam@users.noreply.github.com>
2025-09-22 13:15:03 -0400

    add config loading

--- src/Application/App.php
+++ src/Application/App.php
@@ -13,11 +13,14 @@ class App {
   private static $instance;
   private static Container $container;
   private static ServiceProviderRegistry $serviceProviderRegistry;
+  private static Config $config;
 
   private function __construct(string $rootPath) {
     $this->loadEnv($rootPath);
 
     $this->defineConstants($rootPath);
+
+    $this->loadConfig();
 
     static::$container = new Container();
     static::$serviceProviderRegistry = new ServiceProviderRegistry(static::$container);
@@ -33,16 +36,16 @@ class App {
   }
 
   public static function addServiceProvider(ServiceProvider $provider): void {
-    self::$serviceProviderRegistry->register($provider);
+    static::$serviceProviderRegistry->register($provider);
   }
 
   public static function run(): void {
-    self::$serviceProviderRegistry->boot();
+    static::$serviceProviderRegistry->boot();
 
     $request = new Request();
-    self::$container->instance(Request::class, $request);
+    static::$container->instance(Request::class, $request);
 
-    Response::create("Path requested: " . $request->getPath())->send();
+    Response::create("Path requested: " . $request->getPath() . "\n<pre>" . print_r(App::getConfig()->all(), true) . "</pre>")->send();
   }
 
   public static function getVersion(): string {
@@ -50,11 +53,15 @@ class App {
   }
 
   public static function getContainer(): Container {
-    return self::$container;
+    return static::$container;
+  }
+
+  public static function getConfig(): Config {
+    return static::$config;
   }
 
   public static function getRequest(): Request {
-    return self::$container->get(Request::class);
+    return static::$container->get(Request::class);
   }
 
   private function loadEnv(string $rootPath): void {
@@ -94,6 +101,19 @@ class App {
 
   private function defineConstants(string $rootPath): void {
     define('NEWTRON_ROOT', $rootPath);
+    define('NEWTRON_CONFIG', NEWTRON_ROOT . '/config');
+  }
+
+  private function loadConfig(): void {
+    $files = glob(NEWTRON_CONFIG . '/*.php');
+
+    $items = [];
+    foreach ($files as $file) {
+      $name = basename($file, '.php');
+      $items[$name] = require $file;
+    }
+
+    static::$config = new Config($items);
   }
 
   private function registerServices(): void {
